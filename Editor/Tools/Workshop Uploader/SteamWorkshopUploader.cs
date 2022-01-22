b71145f4af3223b55595711593a185f411a7c7f3
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-22 21:33:44 +1100

    Tool > EditorTool, Updates to Map compiler and resource

--- Editor/Tools/Workshop Uploader/SteamWorkshopUploader.cs
+++ Editor/Tools/Workshop Uploader/SteamWorkshopUploader.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 using UnityEditor;
 using Espionage.Engine.Tools.Editor;
 using Espionage.Engine.Editor;
-using Tool = Espionage.Engine.Tools.Editor.Tool;
 
 namespace Espionage.Engine.Steam.Editor.Tools
 {
-    [Library( "tool.workshop_uploader", Title = "Workshop Uploader", Help = "Publish content to the Steam Workshop", Group = "Publish" ), Icon( EditorIcons.Dashboard ), HelpURL( "https://github.com/Woosh-Crew/Espionage.Engine/wiki" )]
-    public class SteamWorkshopUploader : Tool
-    {
-        [MenuItem( "Tools/Workshop Uploader _F6", false, -150 )]
-        private static void ShowEditor( )
-        {
-            var wind = GetWindow<SteamWorkshopUploader>( );
-        }
-    }
+	[Library( "tool.workshop_uploader", Title = "Workshop Uploader", Help = "Publish content to the Steam Workshop", Group = "Publish" ), Icon( EditorIcons.Dashboard ), HelpURL( "https://github.com/Woosh-Crew/Espionage.Engine/wiki" )]
+	public class SteamWorkshopUploader : EditorTool
+	{
+		[MenuItem( "Tools/Workshop Uploader _F6", false, -150 )]
+		private static void ShowEditor()
+		{
+			var wind = GetWindow<SteamWorkshopUploader>();
+		}
+	}
 }
