2676327ae1c7a35b6d3a3ddf92c6ed6d017509d2
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-01-22 12:42:55 +1100

    Remove blueprints, add singleton component

--- Editor/Tools/Workshop Uploader/SteamWorkshopUploader.cs
+++ Editor/Tools/Workshop Uploader/SteamWorkshopUploader.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using Espionage.Engine.Tools.Editor;
+using Espionage.Engine.Editor;
+using Tool = Espionage.Engine.Tools.Editor.Tool;
 
-namespace Espionage.Engine.Editor.Internal
+namespace Espionage.Engine.Steam.Editor.Tools
 {
-	[Library( "tool.workshop_uploader", Title = "Workshop Uploader", Help = "Publish content to the Steam Workshop", Group = "Publish" )]
-	[Icon( EditorIcons.Dashboard )]
-	[HelpURL( "https://github.com/Woosh-Crew/Espionage.Engine/wiki" )]
-	public class SteamWorkshopUploader : Tool
-	{
-		[MenuItem( "Tools/Workshop Uploader _F6", false, -150 )]
-		private static void ShowEditor()
-		{
-			var wind = GetWindow<SteamWorkshopUploader>();
-		}
-	}
+    [Library( "tool.workshop_uploader", Title = "Workshop Uploader", Help = "Publish content to the Steam Workshop", Group = "Publish" ), Icon( EditorIcons.Dashboard ), HelpURL( "https://github.com/Woosh-Crew/Espionage.Engine/wiki" )]
+    public class SteamWorkshopUploader : Tool
+    {
+        [MenuItem( "Tools/Workshop Uploader _F6", false, -150 )]
+        private static void ShowEditor( )
+        {
+            var wind = GetWindow<SteamWorkshopUploader>( );
+        }
+    }
 }
