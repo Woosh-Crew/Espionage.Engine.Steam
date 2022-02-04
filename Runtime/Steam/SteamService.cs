20830834fa66b79c60cb2f789151ac67a0196e1e
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-02-04 21:35:37 +1100

    Update services to finally use Library System... so Engine init is faster

--- Runtime/Steam/SteamService.cs
+++ Runtime/Steam/SteamService.cs
@@ -6,6 +6,13 @@ namespace Espionage.Engine.Steam
 {
 	internal class SteamService : IService
 	{
+		public Library ClassInfo { get; }
+
+		public SteamService()
+		{
+			ClassInfo = Library.Database[GetType()];
+		}
+
 		public void OnReady()
 		{
 			var appId = Engine.Game is not null && Engine.Game.ClassInfo.Components.TryGet<SteamAttribute>( out var steam ) ? steam.AppId : 1614530;
