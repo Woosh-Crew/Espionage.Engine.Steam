56e6a59522deb2fb49746b9316b618187fe39942
Jake <55055481+Jake-NSW@users.noreply.github.com>
2022-03-01 18:32:05 +1100

    Updates

--- Runtime/Workshop/WorkshopGrabber.cs
+++ Runtime/Workshop/WorkshopGrabber.cs
@@ -11,25 +11,6 @@ namespace Espionage.Engine.Steam
 	internal static class WorkshopGrabber
 	{
 		[Function, Callback( "steam.ready" )]
-		public static async void Grab()
-		{
-			// Grab Maps
-			var items = await Query.All.WhereUserSubscribed().GetPageAsync( 1 );
-
-			if ( !items.HasValue )
-			{
-				return;
-			}
-
-			foreach ( var item in items.Value.Entries )
-			{
-				foreach ( var path in Directory.GetFiles( item.Directory, "*.map", SearchOption.AllDirectories ) )
-				{
-					var map = new Map( new AssetBundleMapProvider( new FileInfo( path ) ) );
-					map.Components.Add( new SteamUgcComponent( item ) );
-					Map.Database.Add( map );
-				}
-			}
-		}
+		public static async void Grab() { }
 	}
 }
