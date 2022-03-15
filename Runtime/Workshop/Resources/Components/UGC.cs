7fdbac6e0f1bd746ec9474aeda6a05a27f6302e8
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-15 14:26:10 +1100

    Updates to work with latest PR merge on main

---
+++ Runtime/Workshop/Resources/Components/UGC.cs
@@ -0,0 +1,25 @@
+﻿using Espionage.Engine.Components;
+using Steamworks;
+using Espionage.Engine.Resources;
+using Steamworks.Data;
+
+namespace Espionage.Engine.Steam.Resources
+{
+	public class UGC : IComponent<Map>, IComponent<IResource>
+	{
+		private PublishedFileId FileId { get; }
+		
+		public UGC( ulong fileId )
+		{
+			FileId = fileId;
+		}
+
+		public void OnAttached( IResource item ) { }
+		
+		public void OnAttached( Map item )
+		{
+			item.Loaded += () => SteamUGC.StartPlaytimeTracking( FileId );
+			item.Unloaded += () => SteamUGC.StopPlaytimeTracking( FileId );
+		}
+	}
+}
