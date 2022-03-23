c59b75bcb474b38074d010ef6c2083a31f3af6b6
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-23 12:49:05 +1100

    Update WorkshopGrabber, to cache all maps.

--- Runtime/Workshop/Resources/Components/UGC.cs
+++ Runtime/Workshop/Resources/Components/UGC.cs
@@ -1,25 +1,25 @@
 ﻿using Espionage.Engine.Components;
 using Steamworks;
 using Espionage.Engine.Resources;
-using Steamworks.Data;
+using Steamworks.Ugc;
 
 namespace Espionage.Engine.Steam.Resources
 {
 	public class UGC : IComponent<Map>, IComponent<IResource>
 	{
-		private PublishedFileId FileId { get; }
-		
-		public UGC( ulong fileId )
+		private Item Item { get; }
+
+		public UGC( Item item )
 		{
-			FileId = fileId;
+			Item = item;
 		}
 
 		public void OnAttached( IResource item ) { }
-		
+
 		public void OnAttached( Map item )
 		{
-			item.Loaded += () => SteamUGC.StartPlaytimeTracking( FileId );
-			item.Unloaded += () => SteamUGC.StopPlaytimeTracking( FileId );
+			item.Loaded += () => SteamUGC.StartPlaytimeTracking( Item.Id );
+			item.Unloaded += () => SteamUGC.StopPlaytimeTracking( Item.Id );
 		}
 	}
 }
