895e0bdb43c2d4ccb5c37957723e2a8f7e7ee659
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-24 22:56:53 +1100

    Update UGC to use new Map Callbacks

--- Runtime/Workshop/Resources/Components/UGC.cs
+++ Runtime/Workshop/Resources/Components/UGC.cs
@@ -2,10 +2,11 @@
 using Steamworks;
 using Espionage.Engine.Resources;
 using Steamworks.Ugc;
+using UnityEngine.SceneManagement;
 
 namespace Espionage.Engine.Steam.Resources
 {
-	public class UGC : IComponent<Map>, IComponent<IResource>
+	public class UGC : IComponent<Map>, Map.ICallbacks, IComponent<IResource>
 	{
 		private Item Item { get; }
 
@@ -15,11 +16,29 @@ namespace Espionage.Engine.Steam.Resources
 		}
 
 		public void OnAttached( IResource item ) { }
+		public void OnAttached( Map item ) { }
 
-		public void OnAttached( Map item )
+		// Map
+
+		public void OnLoad( Scene scene )
 		{
-			item.Loaded += () => SteamUGC.StartPlaytimeTracking( Item.Id );
-			item.Unloaded += () => SteamUGC.StopPlaytimeTracking( Item.Id );
+			SteamUGC.StartPlaytimeTracking( Item.Id );
+		}
+
+		public void OnUnload()
+		{
+			SteamUGC.StopPlaytimeTracking( Item.Id );
+		}
+
+		public ILoadable Inject()
+		{
+			if ( Item.IsInstalled )
+			{
+				return null;
+			}
+
+			// Download the item if its not installed
+			return Operation.Create( Item.DownloadAsync(), $"Downloading {Item.Title} by {Item.Owner.Name}" );
 		}
 	}
 }
