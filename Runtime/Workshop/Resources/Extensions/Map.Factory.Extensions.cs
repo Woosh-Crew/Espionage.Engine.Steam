cc52a3b5c0965659b459c8c6e3e1048d980aae56
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2022-03-26 13:36:57 +1100

    Update workshop to have its own ILoadable Download callback, so it displays correct progress. Add Map.Setup.Workshop(item). Add workshop.search and workshop.load terminal commands

--- Runtime/Workshop/Resources/Extensions/Map.Factory.Extensions.cs
+++ Runtime/Workshop/Resources/Extensions/Map.Factory.Extensions.cs
@@ -7,12 +7,18 @@ namespace Espionage.Engine.Steam
 {
 	public static class MapFactoryExtensions
 	{
-		public static Map.Builder Workshop( this Map.Factory factory, Item item )
+		public static Map.Builder? Workshop( this Map.Factory factory, Item item )
 		{
 			var extensions = Library.Database.GetAll<Map.File>().Select( e => e.Components.Get<FileAttribute>()?.Extension ).ToArray();
 			var path = Files.Pathing.All( item.Directory, extensions ).FirstOrDefault();
 
-			return Map.Setup.Path( path )
+			if ( path == null )
+			{
+				Dev.Log.Warning( $"{item.Title} did not contain a valid map." );
+				return null;
+			}
+
+			return Map.Setup.Path( path )?
 				.Meta( item.Title, item.Description, item.Owner.Name )
 				.Origin( "Steam Workshop" )
 				.With<UGC>( new( item ) );
--- Runtime/Workshop/Workshop.cs
+++ Runtime/Workshop/Workshop.cs
@@ -1,4 +1,8 @@
-﻿using Espionage.Engine.Resources;
+﻿using System;
+using System.Linq;
+using Espionage.Engine.Logging;
+using Espionage.Engine.Resources;
+using Steamworks;
 using Steamworks.Ugc;
 
 namespace Espionage.Engine.Steam
@@ -16,22 +20,25 @@ namespace Espionage.Engine.Steam
 
 		public static ILoadable Download( Item item )
 		{
+			Assert.IsNull( item );
+
 			if ( item.IsInstalled )
 			{
 				Dev.Log.Warning( $"Item [{item.Title}] was already installed." );
 				return null;
 			}
 
-			var download = item.DownloadAsync();
-			item.Subscribe();
-
-			return Operation.Create( download, $"Downloading Workshop Item {item.Title}" );
+			return new ItemDownloadCallback( item );
 		}
 
 		[Function, Callback( "steam.ready" )]
 		private static async void Grab()
 		{
-			var query = Query.ItemsReadyToUse.WhereUserSubscribed();
+			var query = Query.ItemsReadyToUse
+				.WithTag( "Maps" )
+				.MatchAnyTag()
+				.WhereUserSubscribed();
+
 			var page = 1;
 
 			// Get all pages
@@ -49,8 +56,103 @@ namespace Espionage.Engine.Steam
 
 				foreach ( var item in items.Value.Entries )
 				{
-					Map.Setup.Workshop( item ).Build();
+					Map.Setup.Workshop( item )?.Build();
 				}
+			}
+		}
+
+		//
+		// ILoadable Callbacks
+		//
+
+		private class ItemDownloadCallback : ILoadable
+		{
+			private Item Item { get; }
+
+			public ItemDownloadCallback( Item item )
+			{
+				Item = item;
+			}
+
+			// Loadable
+
+			public float Progress { get; private set; }
+			public string Text => $"Downloading Workshop Item {Item.Title}";
+
+			private void Update( float value )
+			{
+				Progress = value;
+			}
+
+			public async void Load( Action loaded )
+			{
+				try
+				{
+					// Subscribe and Download
+					await Item.Subscribe();
+					await Item.DownloadAsync( Update );
+				}
+				catch ( Exception e )
+				{
+					Dev.Log.Exception( e );
+				}
+
+				loaded.Invoke();
+			}
+		}
+
+		//
+		// Commands
+		//
+
+		[Function( "workshop.load" ), Terminal]
+		private static async void Download_CMD( string name )
+		{
+			Item? item;
+
+			if ( ulong.TryParse( name, out var value ) )
+			{
+				item = await SteamUGC.QueryFileAsync( value );
+			}
+			else
+			{
+				var page = await Query.ItemsReadyToUse.WhereSearchText( name ).GetPageAsync( 1 );
+
+				if ( !page.HasValue || page.Value.ResultCount == 0 )
+				{
+					Dev.Log.Warning( "No Item's could be found." );
+					return;
+				}
+
+				item = page.Value.Entries.FirstOrDefault();
+			}
+
+			if ( item == null )
+			{
+				Dev.Log.Warning( "Couldn't find workshop item" );
+				return;
+			}
+
+			Engine.Game.Loader.Start(
+				new Loader.Request( Download( item.Value ) ),
+				new Loader.Request( () => Map.Setup.Workshop( item.Value )?.Build() )
+			);
+		}
+
+		[Function( "workshop.search" ), Terminal]
+		private static async void Search_CMD( string name )
+		{
+			var page = await Query.ItemsReadyToUse.WhereSearchText( name ).GetPageAsync( 1 );
+
+			if ( !page.HasValue || page.Value.ResultCount == 0 )
+			{
+				Dev.Log.Warning( "No Item's could be found." );
+				return;
+			}
+
+			foreach ( var item in page.Value.Entries )
+			{
+				Dev.Log.Add( new() { Message = $"{item.Title} [{item.Owner.Name}]", StackTrace = item.Description } );
 			}
 		}
 	}
